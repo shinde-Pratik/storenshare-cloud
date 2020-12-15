2e5b96cdf5f967ac9ff6a775a204c11100751ec8
Pratik <pratikshinde1902@gmail.com>
2020-12-15 18:27:19 +0530

    add cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ connectDB();
 //cors
 
 const corsOptions = {
-    origin : process.env.ALLOWED_CLIENTS.split(',')
+    origin: process.env.ALLOWED_CLIENTS.split(',')
 
 }
 app.use(cors(corsOptions));
